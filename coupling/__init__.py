e5b8c328dee0fe59700080d495aeb46e1076850f
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-02-14 18:16:09 +0100

    added __init__.py

---
+++ coupling/__init__.py
@@ -0,0 +1,9 @@
+"""
+.. moduleauthor:: Francesco Witte <francesco.witte@hs-flensburg.de>
+
+"""
+__version__ = '0.0.1 dev'
+
+from coupling import coupling as cp
+from coupling import powerplant as pp
+from coupling import geostorage as gs
